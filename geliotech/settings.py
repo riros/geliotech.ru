6c48b2638d631a58baa5614ffdf151b04ce0669f
Riros ivanvalenkov@gmail.com <i.valenkov@itsirius.ru>
2017-06-06 12:46:25 +0300

    save

--- geliotech/settings.py
+++ geliotech/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'b98y_dzlhljr@9maj5gl7w@%c^xoxjgrpwst1%gsh%yg4l9$89'
 DEBUG = True
 
 ALLOWED_HOSTS = ['geliotech.ru', '*']
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'geliotech.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
@@ -115,16 +110,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = 'http://localhost/statics/geliotech/static/'
 STATIC_ROOT = '/var/www/localhost/htdocs/statics/geliotech/static/'
 
-
-MEDIA_ROOT = "/var/www/localhost/htdocs/statics/geliotech/media/"
 MEDIA_URL = 'http://localhost/statics/geliotech/media/'
+MEDIA_ROOT = '/var/www/localhost/htdocs/statics/geliotech/media/'
 
 SRC_SITE = "http://ekoproekt-energo.ru/"
 
