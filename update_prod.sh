0976a2ae9123f9ccaeeb8960838847397254e69c
Riros ivanvalenkov@gmail.com <i.valenkov@itsirius.ru>
2017-06-06 22:16:56 +0300

    save

--- update_prod.sh
+++ update_prod.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 git pull
-sudo python3 manage_prod.py collectstatic --silent
+sudo python3 manage_prod.py collectstatic --noinput
 sudo python3 manage_prod.py migrate
 sudo systemctl restart uwsgi
