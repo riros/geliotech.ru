c5b35612ce5116e6f6347ae8f3aaf8854b372965
Riros ivanvalenkov@gmail.com <i.valenkov@itsirius.ru>
2017-06-11 20:41:13 +0300

    fix

--- update_prod.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-git pull
-sudo python3 manage_prod.py collectstatic --noinput
-sudo python3 manage_prod.py migrate
-sudo systemctl restart uwsgi-app@geliotech
-sudo systemctl restart nginx
-
