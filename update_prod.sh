a2eb0df6b0e1a664263408170dafa2ff6a98d736
Riros ivanvalenkov@gmail.com <i.valenkov@itsirius.ru>
2017-06-11 18:43:11 +0300

    save

--- update_prod.sh
+++ update_prod.sh
@@ -3,4 +3,6 @@
 git pull
 sudo python3 manage_prod.py collectstatic --noinput
 sudo python3 manage_prod.py migrate
-sudo systemctl restart uwsgi
+sudo systemctl restart uwsgi-app@geliotech
+sudo systemctl restart nginx
+
